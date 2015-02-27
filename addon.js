91e956e661be872d94ce56aca9f32a74c053d8f7
Jeff Schilling <jeff.schilling@q2ebanking.com>
2015-02-26 23:51:45 -0500

    Always prefer the ember-cli version of ember-template-compiler when used as an ember-cli addon

--- addon.js
+++ addon.js
@@ -1,4 +1,7 @@
 var inlineTemplateCompiler = require('./index');
+var path = require('path');
+// for ember-cli always use the bower version of the ember template compiler
+var compiler = require(path.join(process.cwd(),'./bower_components/ember/ember-template-compiler.js'));
 
 module.exports = {
   name: 'broccoli-ember-inline-template-compiler',
@@ -9,7 +12,9 @@ module.exports = {
       name: 'broccoli-ember-inline-template-compiler',
       ext: 'js',
       toTree: function(tree) {
-        return inlineTemplateCompiler(tree);
+        return inlineTemplateCompiler(tree, { 
+          compiler: compiler
+        });
       }
     });
   }
